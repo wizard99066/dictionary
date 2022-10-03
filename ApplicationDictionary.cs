424382e662461eb94a9941824bad8b985d75e80c
rita <ritavlas@yandex.ru>
2022-10-03 12:03:44 +0300

    Добавьте файлы проекта.

---
+++ ApplicationDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary
+{
+    internal class ApplicationDictionary<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> _dictionary;
+
+        public ApplicationDictionary()
+        {
+            _dictionary = new Dictionary<TKey, TValue>();
+        }
+        public void Add(TKey key, TValue value)
+        {
+            _dictionary.Add(key, value);
+        }
+
+        public void Remove(TKey key)
+        {
+            _dictionary.Remove(key, out TValue value);
+        }
+
+        public void ListOfKey()
+        {
+            foreach (var temp in _dictionary)
+            {
+                Console.WriteLine(temp.Key);
+            }
+        }
+
+        public TValue GetValueByKey(TKey key)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                _dictionary.TryGetValue(key, out TValue? value);
+
+                return value;
+            }
+            else
+            {
+                throw new Exception("This key is not in the dictionary");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDictionary<string, string> dict = new ApplicationDictionary<string, string>();
+
+            dict.Add("one", "один");
+            dict.Add("two", "два");
+            dict.Add("pen", "ручка");
+            dict.ListOfKey();
+            Console.WriteLine();
+            dict.Remove("pen");
+            dict.ListOfKey();
+            dict.GetValueByKey("one");
+        }
+    }
+}
